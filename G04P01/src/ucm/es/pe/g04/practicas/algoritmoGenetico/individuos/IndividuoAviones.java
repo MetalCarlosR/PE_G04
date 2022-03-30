7b0ea9c24a58276a932d2a3dbc5dda547c7aa486
Carlos Romero <mcarlos.romerog99@gmail.com>
2022-03-30 12:11:18 +0200

    fitness correcto

--- G04P01/src/ucm/es/pe/g04/practicas/algoritmoGenetico/individuos/IndividuoAviones.java
+++ G04P01/src/ucm/es/pe/g04/practicas/algoritmoGenetico/individuos/IndividuoAviones.java
@@ -14,22 +14,22 @@ public class IndividuoAviones extends Individuo<Integer>{
     @Override
     public void init() {
         tamGenes = new int[IndividuoAvionesData.numAviones];
-//        cromosoma = new Integer[]{7, 8, 9, 10, 11, 6, 5, 4, 3, 2, 1, 0};
-        cromosoma = new Integer[IndividuoAvionesData.numAviones];
-        for (int i = 0; i < IndividuoAvionesData.numAviones; i++) {
-            cromosoma[i] = i;
-            tamGenes[i] = 1;
-        }
-        for (int i = cromosoma.length - 1; i > 0; i--)
-        {
-            int index = rand.nextInt(i + 1);
-            if(index == i)
-                continue;
-            // Simple swap
-            int a = cromosoma[index];
-            cromosoma[index] = cromosoma[i];
-            cromosoma[i] = a;
-        }
+        cromosoma = new Integer[]{7, 8, 9, 10, 11, 6, 5, 4, 3, 2, 1, 0};
+//        cromosoma = new Integer[IndividuoAvionesData.numAviones];
+//        for (int i = 0; i < IndividuoAvionesData.numAviones; i++) {
+//            cromosoma[i] = i;
+//            tamGenes[i] = 1;
+//        }
+//        for (int i = cromosoma.length - 1; i > 0; i--)
+//        {
+//            int index = rand.nextInt(i + 1);
+//            if(index == i)
+//                continue;
+//            // Simple swap
+//            int a = cromosoma[index];
+//            cromosoma[index] = cromosoma[i];
+//            cromosoma[i] = a;
+//        }
     }
 
     @Override
@@ -64,6 +64,7 @@ public class IndividuoAviones extends Individuo<Integer>{
             }
             lastAvionPista[pista] = a.tipo;
             lastTime[pista] = tiempo;
+//            acc += Math.pow(tiempo - a.tel[pista], 2);
             acc += Math.pow(tiempo - Array.min(a.tel), 2);
         }
 
