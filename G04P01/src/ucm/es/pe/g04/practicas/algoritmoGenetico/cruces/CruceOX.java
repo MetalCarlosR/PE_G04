283b2ae3ec320257485af2fc059db69f57b1113b
David Godoy <dagodoy@ucm.es>
2022-03-31 23:34:43 +0200

    OX y OX_PP

--- G04P01/src/ucm/es/pe/g04/practicas/algoritmoGenetico/cruces/CruceOX.java
+++ G04P01/src/ucm/es/pe/g04/practicas/algoritmoGenetico/cruces/CruceOX.java
@@ -2,10 +2,45 @@ package ucm.es.pe.g04.practicas.algoritmoGenetico.cruces;
 
 import ucm.es.pe.g04.practicas.algoritmoGenetico.individuos.Individuo;
 
+import java.util.ArrayList;
+
 public class CruceOX extends Cruce{
     @Override
     protected void cruzar(Individuo individuo1, Individuo individuo2) {
+        int length = individuo1.cromosoma.length;
+        var aux1 = individuo1.cromosoma.clone();
+        var aux2 = individuo2.cromosoma.clone();
 
+        int p1 = r.nextInt(length), p2 = r.nextInt(length);
+        if (p1 > p2){
+            int aux = p1;
+            p1 = p2;
+            p2 = aux;
+        }
+        if (p1 == p2) return;
+        ArrayList<Object> a1 = new ArrayList<Object>(), a2 = new ArrayList<Object>();
+        for (int i = p1; i < p2; i++) {
+            a1.add(aux2[i]);
+            a2.add(aux1[i]);
+            individuo1.cromosoma[i] = aux2[i];
+            individuo2.cromosoma[i] = aux1[i];
+        }
+        int i = p2, i1 = p2, i2 = p2;
+
+        while (i1 < p1 + length || i2 < p2 + length){
+            if (!a1.contains(aux1[i])){
+                individuo1.cromosoma[i1 % length] = aux1[i];
+                i1++;
+            }
+
+            if (!a2.contains(aux2[i])){
+                individuo2.cromosoma[i2 % length] = aux2[i];
+                i2++;
+            }
+
+            i++;
+            i %= length;
+        }
     }
 
     public String toString() {
--- G04P01/src/ucm/es/pe/g04/practicas/algoritmoGenetico/cruces/CruceOX_PP.java
+++ G04P01/src/ucm/es/pe/g04/practicas/algoritmoGenetico/cruces/CruceOX_PP.java
@@ -2,10 +2,49 @@ package ucm.es.pe.g04.practicas.algoritmoGenetico.cruces;
 
 import ucm.es.pe.g04.practicas.algoritmoGenetico.individuos.Individuo;
 
+import java.util.ArrayList;
+
 public class CruceOX_PP extends Cruce{
+    private int _numCruce = 3;
     @Override
     protected void cruzar(Individuo individuo1, Individuo individuo2) {
+        int length = individuo1.cromosoma.length;
+        var aux1 = individuo1.cromosoma.clone();
+        var aux2 = individuo2.cromosoma.clone();
 
+        ArrayList<Object> elems1 = new ArrayList<Object>(_numCruce);
+        ArrayList<Object> elems2 = new ArrayList<Object>(_numCruce);
+        ArrayList<Integer> lista = new ArrayList<Integer>(length);
+        for(int i = 0; i < length; i++) {
+            lista.add(i);
+        }
+        for (int i = 0; i < _numCruce; i++){
+            int aux =  lista.remove(r.nextInt(lista.size()));
+            individuo1.cromosoma[aux] = aux2[aux];
+            individuo2.cromosoma[aux] = aux1[aux];
+            elems1.add(individuo1.cromosoma[aux]);
+            elems2.add(individuo2.cromosoma[aux]);
+        }
+        for (int i = 0, i1 = 0, i2 = 0; i < length; i++) {
+//            i1 %= length;
+//            i2 %= length;
+            //Significa que no son posiciones vetadas
+            if (lista.contains(i)){
+                while (elems1.contains(aux1[i1])){
+                    i1++;
+//                    i1 %= length;
+                }
+                individuo1.cromosoma[i] = aux1[i1];
+
+                while (elems2.contains(aux2[i2])){
+                    i2++;
+//                    i2 %= length;
+                }
+                individuo2.cromosoma[i] = aux2[i1];
+
+                i1++; i2++;
+            }
+        }
     }
 
     public String toString() {
